0d1ee04b0c74668ee26d9eee23528e0a9aa07c19
davidtnfsh <davidtnfsh@gmail.com>
2022-10-07 23:14:38 +0800

    janky frontend

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Minter from "./Minter";
+
+function App() {
+  return (
+    <div className="App">
+      <Minter></Minter>
+      <center>1. DAO 願景 + 如何加入共同治理</center>
+      <center>2. 要能直接在頁面上選票種跟用幣買票</center>
+      <center>3. 演講時程</center>
+      <center>4. 能在頁面上就讀到該錢包的票（NFT）</center>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Minter.js
@@ -0,0 +1,76 @@
+import { useEffect, useState } from "react";
+import { connectWallet, getCurrentWalletConnected } from "./util/interact.js";
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+const web3 = createAlchemyWeb3(alchemyKey);
+
+const Minter = (props) => {
+  const [walletAddress, setWallet] = useState("");
+  const [status, setStatus] = useState("");
+
+  const [nfts, setNFTs] = useState({ result: [] });
+  console.log(status, nfts);
+  const [NftDoms, setNftDoms] = useState(<p></p>);
+  async function requestNftData() {
+    const { address, status } = await getCurrentWalletConnected();
+
+    setWallet(address);
+    setStatus(status);
+    addWalletListener();
+  }
+  useEffect(requestNftData, []);
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("👆🏽 Write a message in the text-field above.");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a
+            target="_blank"
+            href={`https://metamask.io/download.html`}
+            rel="noreferrer"
+          >
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>
+      );
+    }
+  }
+
+  const connectWalletPressed = async () => {
+    const walletResponse = await connectWallet();
+    setStatus(walletResponse.status);
+    setWallet(walletResponse.address);
+    console.log("connectWalletPressed!");
+    requestNftData();
+  };
+
+  return (
+    <div className="Minter">
+      <button id="walletButton" onClick={connectWalletPressed}>
+        {walletAddress.length > 0 ? (
+          "Connected: " +
+          String(walletAddress).substring(0, 6) +
+          "..." +
+          String(walletAddress).substring(38)
+        ) : (
+          <span>Connect Wallet</span>
+        )}
+      </button>
+      {NftDoms}
+    </div>
+  );
+};
+
+export default Minter;
