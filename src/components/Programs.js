b65a8f9a9dc51d18188bb9a1fa117c994fe124d1
davidtnfsh <davidtnfsh@gmail.com>
2022-12-10 10:52:00 +0800

    chores: update schedule

--- src/components/Programs.js
+++ src/components/Programs.js
@@ -19,78 +19,78 @@ function Programs() {
           <p className="name">David</p>
         </li>
         <li>
-          <p className="time">09:40~10:10</p>
+          <p className="time">09:55~10:25</p>
           <p className="topic">區塊鏈 Layer 2 解決方案簡介</p>
           <img src="mic.png" alt="" />
           <p className="name">Vincent</p>
         </li>
         <li>
-          <p className="time">10:20~10:50</p>
+          <p className="time">10:35~11:05</p>
           <p className="topic">ERC721RugGripper</p>
           <img src="mic.png" alt="" />
           <p className="name">Justa</p>
         </li>
         <li>
-          <p className="time">11:00~11:30</p>
+          <p className="time">11:15~11:45</p>
           <p className="topic">Applied Zero-Knowledge Proofs for software engineers: case study</p>
           <img src="mic.png" alt="" />
           <p className="name">Alex Kuzmin</p>
         </li>
         <li>
-          <p className="time">11:40~12:10</p>
+          <p className="zone">PM</p>
+        </li>
+        <li>
+          <p className="time">11:45~12:45</p>
+          <p className="event">Lunch break</p>
+        </li>
+        <li>
+          <p className="time">12:45~13:15</p>
           <p className="topic">不需要透過網站且不用透過function即可以執行mint NFT</p>
           <img src="mic.png" alt="" />
           <p className="name">Aaron</p>
         </li>
         <li>
-          <p className="zone">PM</p>
-        </li>
-        <li>
-          <p className="time">12:10~13:10</p>
-          <p className="event">Lunch break</p>
-        </li>
-        <li>
-          <p className="time">13:10~13:40</p>
+          <p className="time">13:25~13:55</p>
           <p className="topic">Aptos 上的 Vesting Wallet</p>
           <img src="mic.png" alt="" />
           <p className="name">Justa</p>
         </li>
         <li>
-          <p className="time">13:50~14:20</p>
+          <p className="time">14:05~14:35</p>
           <p className="topic">現今支付方式比較與加密貨幣的支付解決方案</p>
           <img src="mic.png" alt="" />
           <p className="name">Akira</p>
         </li>
         <li>
-          <p className="time">14:30~15:00</p>
+          <p className="time">14:45~15:15</p>
           <p className="event">Tea Break</p>
         </li>
         <li>
-          <p className="time">15:00~15:20</p>
+          <p className="time">15:25~15:55</p>
           <p className="topic">Lightning Talk</p>
         </li>
         <li>
-          <p className="time">15:30~16:00</p>
+          <p className="time">16:05~16:35</p>
           <p className="topic">ZK 多重宇宙</p>
           <img src="mic.png" alt="" />
           <p className="name">Ray</p>
         </li>
         <li>
-          <p className="time">16:10~16:40</p>
+          <p className="time">16:45~17:15</p>
           <p className="event">Job Fairs</p>
         </li>
         <li>
-          <p className="time">16:40~17:10</p>
+          <p className="time">17:15~17:45</p>
           <p className="topic">Teleport module on Aptos</p>
           <img src="mic.png" alt="" />
           <p className="name">Rick</p>
         </li>
         <li>
-          <p className="time">17:10~17:15</p>
+          <p className="time">17:45~17:50</p>
           <p className="topic">Closing</p>
         </li>
         <li className="pb-28">
-          <p className="time">17:15</p>
+          <p className="time">17:50</p>
           <p className="event">Dinner party for speakers and sponsers</p>
         </li>
       </ul>
